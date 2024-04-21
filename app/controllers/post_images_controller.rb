85473378c689ef753565f88abc578bada9f4e48a
7suzuki <162399385+7suzuki@users.noreply.github.com>
2024-04-21 11:15:33 +0000

    [Finish]4-15

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -12,7 +12,7 @@ class PostImagesController < ApplicationController
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
     @post_image.save
-    redirect_to post_images_path　#投稿一覧へ画面遷移
+    redirect_to post_images_path #投稿一覧へ画面遷移
   end
     
   def index
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,10 +14,10 @@ has_many :post_images, dependent: :destroy
 # ユーザーごとのプロフィール画像をアップロードできるようにする
 has_one_attached :profile_image
 
-def get_profile_image
+def get_profile_image(width, height)
   unless profile_image.attached?
-    file_path = Rails.root.jpin('app/assets/images/sample-author1.jpg')
-    profile_image.attach(io: File.open(file_path), filename: 'default-image.jpge', content_type: 'image/jpeg')
+    file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+    profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
   end
   profile_image.variant(resize_to_limit: [width, height]).processed
 end

bde9d31fe640ab6383680090ce7b71edf9a7af04
7suzuki <162399385+7suzuki@users.noreply.github.com>
2024-04-21 08:58:37 +0000

    [Finish]4-14

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,15 @@ class User < ApplicationRecord
 
 has_many :post_images, dependent: :destroy
 
+# ユーザーごとのプロフィール画像をアップロードできるようにする
+has_one_attached :profile_image
+
+def get_profile_image
+  unless profile_image.attached?
+    file_path = Rails.root.jpin('app/assets/images/sample-author1.jpg')
+    profile_image.attach(io: File.open(file_path), filename: 'default-image.jpge', content_type: 'image/jpeg')
+  end
+  profile_image.variant(resize_to_limit: [width, height]).processed
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
+  get 'homes/about' =>'homes#about', as: 'about'
+# get URL =>アクション名、asパス名
+# get URLのとき/最初のスラッシュは不要  
   
-  
-  resources :post_images, only:[:new, :create, :index, :show, :destroy]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
   # get 'post_images/new'
   # get 'post_images/index'
   # get 'post_images/show'
-  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'homes/about' =>'homes#about', as: 'about'
-# get URL =>アクション名、asパス名
-# get URLのとき/最初のスラッシュは不要
+  resources :users, only: [:show, :edit]
+  # get 'users/show'
+  # get 'users/edit'  
 end

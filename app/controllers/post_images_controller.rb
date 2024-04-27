4767e985bd4e9263c4da3f8c72615754255b295a
7suzuki <162399385+7suzuki@users.noreply.github.com>
2024-04-27 02:51:37 +0000

    [Finish]4-21

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -12,8 +12,11 @@ class PostImagesController < ApplicationController
   def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
-    @post_image.save
-    redirect_to post_images_path #投稿一覧へ画面遷移
+    if @post_image.save
+      redirect_to post_images_path #投稿一覧へ画面遷移
+    else
+      render :new
+    end
   end
     
   def index

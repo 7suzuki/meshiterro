9de81511ee6fe1e4c242c7569faf7b926303e463
7suzuki <162399385+7suzuki@users.noreply.github.com>
2024-06-02 09:47:48 +0000

    [Finish]5-4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
-   before_action :authenticate_user!, except: [:top]
+   before_action :authenticate_user!, except: [:top], unless :admin_controller?
+     
+   end
    before_action :configure_permitted_parameters, if: :devise_controller?
    
    def after_sign_in_path_for(resource)
@@ -13,6 +15,13 @@ class ApplicationController < ActionController::Base
    def after_sign_out_path_for(resource)
        about_path
    end
+   
+   
+   private
+  
+    def admin_controller?
+      self.class.module_parent_name == 'Admin'
+    end
 
   protected
 
